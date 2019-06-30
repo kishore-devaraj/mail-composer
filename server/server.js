ff1a9d2956f409e2db9fcc418c162066a319bf00
Kishore Devaraj <kishore.devaraj@reedexpo.co.uk>
2019-06-30 22:40:12 +0530

    Add support for multiform data

--- server/server.js
+++ server/server.js
@@ -3,30 +3,33 @@ require('./config/config')
 const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
-
-const { originCheck } = require('./middleware/origin-check')
-// const { Client } = require('./models/Client')
-const  {sendMail} = require('./mailer/mailer')
+const {sendMail} = require('./mailer/mailer')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
-  res.send('Mail server is up ;)')
+    res.send('Mail server is up ;)')
 })
 
-app.post('/api/v1', originCheck, (req, res) => {
-  const body = _.pick(req.body, ['firstName','lastName','email','phoneNumber','message'])
-  // let client = new Client(body)
-
-  sendMail(body)
-  res.send('Your details are now travelling on under water sea cables :P')
+app.post('/api/v1', (req, res) => {
+    const body = _.pick(req.body, ['firstName', 'lastName', 'email', 'phoneNumber', 'message'])
+    sendMail(body)
+    res.send('Your details are now travelling on under water sea cables :P')
 })
-
 
 
 app.listen(PORT, (err) => {
-  if (err) return console.log(err)
-  console.log(`Express server started at ${PORT}`)
+    if (err) return console.log(err)
+    console.log(`Express server started at ${PORT}`)
 })
